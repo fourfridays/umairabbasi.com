273a4cb9bb5f9ad5f971d6cb242142bb427d2865
Umair Abbasi <umair.abbasi@fourfridays.com>
2016-10-04 17:16:33 -0500

    updated homepage to streamfield

--- home/models.py
+++ home/models.py
@@ -3,7 +3,18 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailcore import blocks
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
 
 
 class HomePage(Page):
-    pass
+    body = StreamField ([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('rawhtml', blocks.RawHTMLBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
