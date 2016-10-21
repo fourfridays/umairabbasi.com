c09440f4b45780226136caf8708baf71dd1b0992
Umair Abbasi <umair.abbasi@fourfridays.com>
2016-10-21 16:21:00 -0500

    modified static paths in light of new static path

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -14,16 +14,17 @@ module.exports = function(grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
                 compress: {},
                 beautify: true
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: '../umairabbasi/static/js/<%= pkg.name %>.min.js'
-      }
+        },
+        dist: {
+            files: {
+                '../static/js/umairabbasi.min.js': ['<%= concat.dist.dest %>'],
+            }
+        }
     },
     sass: {                              // Task
       dist: {                            // Target
         files: {                         // Dictionary of files
-          '../umairabbasi/static/css/umairabbasi.css': 'src/umairabbasi.scss'
+          '../static/css/umairabbasi.css': 'src/umairabbasi.scss'
           //'//Volumes/autopub/iamahornet.com/global/css/main.css': 'src/main.scss',
           //'//Volumes/autopub/www.emporia.edu/global/css/main.css': 'src/main.scss'      // 'destination': 'source'
         }
