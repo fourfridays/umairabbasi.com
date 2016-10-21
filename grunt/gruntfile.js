e8d101c150e3c748e56b251700b75132a667ae61
Umair Abbasi <umair.abbasi@fourfridays.com>
2016-10-21 16:20:32 -0500

    installed concat and configured

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -3,6 +3,12 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: ['src/jquery-2.2.4.js', 'node_modules/bootstrap-sass/assets/javascripts/bootstrap/transition.js', 'node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse.js', 'src/umairabbasi.js'],
+                dest: 'dist/js/umairabbasi.js'
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -26,7 +32,7 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: 'src/*.*',
-        tasks: ['uglify', 'sass'],
+        tasks: ['concat', 'uglify', 'sass'],
         options: {
           livereload: true
         },
@@ -35,12 +41,13 @@ module.exports = function(grunt) {
     });
 
     // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
 
-    grunt.registerTask('default', ['uglify', 'sass', 'watch']);
+    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'watch']);
 
 };
