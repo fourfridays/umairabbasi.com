6d53f07704d352efdbf74029e6d41674e0d409ab
Umair Abbasi <umair.abbasi@fourfridays.com>
2017-10-04 11:07:19 +0000

    delete gunicorn-conf file

--- gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-bind = "127.0.0.1:9001"
-errorlog = '/Users/fourfridays/Sites/webservices/logs/gunicorn-error.log'
-accesslog = '/Users/fourfridays/Sites/webservices/logs/gunicorn-access.log'
-loglevel = 'debug'
-workers = 2
