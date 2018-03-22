4ff92b744f2d03474a623d7a47a8c66e4156dddb
fourfridays <umair.abbasi@fourfridays.com>
2018-03-22 00:23:17 +0000

    production push

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -44,8 +44,7 @@ module.exports = function(grunt) {
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.
-loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
