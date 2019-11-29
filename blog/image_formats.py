8172c3e2bdbbcb025092dd720e908bd90bba7a0d
Umair Abbasi <umair.abbasi@fourfridays.com>
2019-11-28 19:54:20 -0800

    divio prep

--- blog/image_formats.py
+++
@@ -1,4 +0,0 @@
-# image_formats.py
-from wagtail.images.formats import Format, register_image_format
-
-register_image_format(Format('full-width', 'Full-Width', 'richtext-image full-width img-fluid img-thumbnail mx-auto d-block', 'width-1200'))
--- referral_links/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyReferralLinksConfig(AppConfig):
-    name = 'my_referral_links'
