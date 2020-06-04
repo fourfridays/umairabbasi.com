2d028144cbdf97df87971e0764d934fd7d8836c2
Umair Abbasi <umair.abbasi@fourfridays.com>
2020-06-03 21:30:09 -0700

    grunt setup

---
+++ grunt/gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                src: ['node_modules/jquery/dist/jquery.slim.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/bootstrap/js/dist/util.js', 'node_modules/bootstrap/js/dist/collapse.js', 'node_modules/bootstrap/js/dist/dropdown.js', 'src/umairabbasi.js'],
+                dest: 'dist/umairabbasi.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                mangle: true,
+                compress: true,
+                beautify: false
+        },
+        dist: {
+            files: {
+                //'/home/umairabbasi/sites/umairabbasi/static/js/umairabbasi.min.js': ['<%= concat.dist.dest %>'],
+                '../static/js/umairabbasi.min.js': ['<%= concat.dist.dest %>'],
+            }
+        }
+    },
+    sass: {                              // Task
+      dist: {                            // Target
+        files: {                         // Dictionary of files
+          //'/home/umairabbasi/sites/umairabbasi/static/css/umairabbasi.css': 'src/umairabbasi.scss'
+          'dist/umairabbasi.css': 'src/umairabbasi.scss'
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'dist',
+          src: ['*.css', '!*.min.css'],
+          dest: '../static/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      scripts: {
+        files: 'src/*.*',
+        tasks: ['concat', 'uglify', 'sass', 'cssmin'],
+        // tasks: ['sass'],
+        options: {
+          livereload: true
+        },
+      },
+    }
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+
+    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'cssmin', 'watch']);
+    // grunt.registerTask('default', ['sass', 'watch']);
+
+};
