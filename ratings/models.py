4dc1230302317deb12e55b132840f67810418041
Umair Abbasi <umair.abbasi@fourfridays.com>
2022-09-13 20:21:38 -0700

    media ratings template updates

--- ratings/models.py
+++ ratings/models.py
@@ -46,15 +46,15 @@ class MoviesIndexPage(RoutablePageMixin, Page):
 
     def get_context(self, request):
         context = super(MoviesIndexPage, self).get_context(request)
-        context['movie_pages'] = MoviePage.objects.live().filter(rating__gte=7).order_by('-rating', '-release_date')
-        context['count'] = context['movie_pages'].count()
+        context['media'] = MoviePage.objects.live().filter(rating__gte=7).order_by('-rating', '-release_date')
+        context['count'] = context['media'].count()
         return context
 
     @route(r'^all/$')
     def get_all_movies(self, request):
         context = super(MoviesIndexPage, self).get_context(request)
-        context['movies'] = MoviePage.objects.live().order_by('title')
-        context['count'] = context['movies'].count()
+        context['media'] = MoviePage.objects.live().order_by('title')
+        context['count'] = context['media'].count()
         return render(request, 'ratings/get_all_movies.html', context)
 
 
@@ -95,15 +95,15 @@ class TvIndexPage(RoutablePageMixin, Page):
 
     def get_context(self, request):
         context = super(TvIndexPage, self).get_context(request)
-        context['tv_pages'] = TvPage.objects.live().filter(rating__gte=7).order_by('-rating', '-release_date')
-        context['count'] = context['tv_pages'].count()
+        context['media'] = TvPage.objects.live().filter(rating__gte=7).order_by('-rating', '-release_date')
+        context['count'] = context['media'].count()
         return context
 
     @route(r'^all/$')
     def get_all_tv_shows(self, request):
         context = super(TvIndexPage, self).get_context(request)
-        context['tv_shows'] = TvPage.objects.live().order_by('title')
-        context['count'] = context['tv_shows'].count()
+        context['media'] = TvPage.objects.live().order_by('title')
+        context['count'] = context['media'].count()
         return render(request, 'ratings/get_all_tv_shows.html', context)
 
 
