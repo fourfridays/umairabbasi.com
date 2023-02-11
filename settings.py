f2a9472171976d29d45827e23a1287ba35736af8
fourfridays <umair.abbasi@fourfridays.com>
2023-02-10 19:01:03 -0500

    black formatter

--- settings.py
+++ settings.py
@@ -15,118 +15,109 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # for guidance on managing these settings.
 
 INSTALLED_APPS = [
-    'anymail',
-    'blog',
-    'fontawesomefree',
-    'page',
-    'ratings',
-    'taxonomy',
-
-    'django.contrib.sitemaps',
-
-    'wagtail.contrib.table_block',
-
-    'wagtail.contrib.forms',
-    'wagtail.contrib.redirects',
-    'wagtail.contrib.modeladmin',
+    "anymail",
+    "blog",
+    "fontawesomefree",
+    "page",
+    "ratings",
+    "taxonomy",
+    "django.contrib.sitemaps",
+    "wagtail.contrib.table_block",
+    "wagtail.contrib.forms",
+    "wagtail.contrib.redirects",
+    "wagtail.contrib.modeladmin",
     "wagtail.contrib.routable_page",
-    'wagtail.embeds',
-    'wagtail.sites',
-    'wagtail.users',
-    'wagtail.snippets',
-    'wagtail.documents',
-    'wagtail.images',
-    'wagtail.search',
-    'wagtail.admin',
-    'wagtail',
-
-    'modelcluster',
-    'taggit',
-
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.sites',
-    'django.contrib.staticfiles',
-
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+    "wagtail.embeds",
+    "wagtail.sites",
+    "wagtail.users",
+    "wagtail.snippets",
+    "wagtail.documents",
+    "wagtail.images",
+    "wagtail.search",
+    "wagtail.admin",
+    "wagtail",
+    "modelcluster",
+    "taggit",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.sites",
+    "django.contrib.staticfiles",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'wagtail.contrib.legacy.sitemiddleware.SiteMiddleware',
-    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "wagtail.contrib.legacy.sitemiddleware.SiteMiddleware",
+    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
-sentry_dsn = os.environ.get('SENTRY_DSN', '')
+sentry_dsn = os.environ.get("SENTRY_DSN", "")
 if sentry_dsn:
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
 
     def ignore_disallowedhost(event, hint):
-        if event.get('logger', None) == 'django.security.DisallowedHost':
+        if event.get("logger", None) == "django.security.DisallowedHost":
             return None
         return event
 
     sentry_sdk.init(
         dsn=sentry_dsn,
-        before_send = ignore_disallowedhost,
-        integrations = [DjangoIntegration()],
-        release=os.environ.get('GIT_COMMIT', 'develop'),
-        environment=os.environ.get('STAGE', 'local'),
-        traces_sample_rate = 0.2,
+        before_send=ignore_disallowedhost,
+        integrations=[DjangoIntegration()],
+        release=os.environ.get("GIT_COMMIT", "develop"),
+        environment=os.environ.get("STAGE", "local"),
+        traces_sample_rate=0.2,
     )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = "urls"
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '<a string of random characters>')
+SECRET_KEY = os.environ.get("SECRET_KEY", "<a string of random characters>")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG') == "True"
+DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
 
-DIVIO_DOMAIN = os.environ.get('DOMAIN', '')
+DIVIO_DOMAIN = os.environ.get("DOMAIN", "")
 DIVIO_DOMAIN_ALIASES = [
-    d.strip()
-    for d in os.environ.get('DOMAIN_ALIASES', '').split(',')
-    if d.strip()
+    d.strip() for d in os.environ.get("DOMAIN_ALIASES", "").split(",") if d.strip()
 ]
 DIVIO_DOMAIN_REDIRECTS = [
-    d.strip()
-    for d in os.environ.get('DOMAIN_REDIRECTS', '').split(',')
-    if d.strip()
+    d.strip() for d in os.environ.get("DOMAIN_REDIRECTS", "").split(",") if d.strip()
 ]
 
 ALLOWED_HOSTS = [DIVIO_DOMAIN] + DIVIO_DOMAIN_ALIASES + DIVIO_DOMAIN_REDIRECTS
 
-CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS', default='https://umairabbasi.com')]
+CSRF_TRUSTED_ORIGINS = [
+    os.environ.get("CSRF_TRUSTED_ORIGINS", default="https://umairabbasi.com")
+]
 
 # Redirect to HTTPS by default, unless explicitly disabled
-SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT') != "False"
+SECURE_SSL_REDIRECT = os.environ.get("SECURE_SSL_REDIRECT") != "False"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -134,8 +125,8 @@ TEMPLATES = [
 
 # Authentication Backends
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 )
 
 # Search Backends
@@ -146,124 +137,120 @@ AUTHENTICATION_BACKENDS = (
 # }
 
 WAGTAILSEARCH_BACKENDS = {
-    'default': {
-        'BACKEND': 'wagtail.search.backends.elasticsearch7',
-        'URLS': os.environ.get('ELASTIC_SEARCH_URL', None),
-        'INDEX': 'umairabbasi',
-        'AUTO_UPDATE': False,
-        'ATOMIC_REBUILD': True,
-        'TIMEOUT': 5,
-        'OPTIONS': {},
-        'INDEX_SETTINGS': {
-            'settings': {
-                'index': {
-                    'number_of_shards': 1,
+    "default": {
+        "BACKEND": "wagtail.search.backends.elasticsearch7",
+        "URLS": os.environ.get("ELASTIC_SEARCH_URL", None),
+        "INDEX": "umairabbasi",
+        "AUTO_UPDATE": False,
+        "ATOMIC_REBUILD": True,
+        "TIMEOUT": 5,
+        "OPTIONS": {},
+        "INDEX_SETTINGS": {
+            "settings": {
+                "index": {
+                    "number_of_shards": 1,
                 },
             },
         },
     }
 }
 
-WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = "wsgi.application"
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite://:memory:')
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite://:memory:")
 
-DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 USE_I18N = True
 USE_TZ = True
 
 STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
-STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = ["static"]
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
 # DEFAULT_FILE_STORAGE is configured using DEFAULT_STORAGE_DSN
 
 # read the setting value from the environment variable
-DEFAULT_STORAGE_DSN = os.environ.get('DEFAULT_STORAGE_DSN')
+DEFAULT_STORAGE_DSN = os.environ.get("DEFAULT_STORAGE_DSN")
 
 # dsn_configured_storage_class() requires the name of the setting
-DefaultStorageClass = dsn_configured_storage_class('DEFAULT_STORAGE_DSN')
+DefaultStorageClass = dsn_configured_storage_class("DEFAULT_STORAGE_DSN")
 
 # Django's DEFAULT_FILE_STORAGE requires the class name
-DEFAULT_FILE_STORAGE = 'settings.DefaultStorageClass'
+DEFAULT_FILE_STORAGE = "settings.DefaultStorageClass"
 
 # only required for local file storage and serving, in development
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join('/data/media/')
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join("/data/media/")
 
-WAGTAIL_SITE_NAME = 'Umair Abbasi'
-WAGTAILADMIN_BASE_URL = 'https://umairabbasi.com/'
+WAGTAIL_SITE_NAME = "Umair Abbasi"
+WAGTAILADMIN_BASE_URL = "https://umairabbasi.com/"
 
-FLICKR_API_KEY = os.getenv('FLICKR_API_KEY', default='')
-FLICKR_API_SECRET = os.getenv('FLICKR_API_SECRET', default='')
-FLICKR_API_USER = os.getenv('FLICKR_API_USER', default='')
+FLICKR_API_KEY = os.getenv("FLICKR_API_KEY", default="")
+FLICKR_API_SECRET = os.getenv("FLICKR_API_SECRET", default="")
+FLICKR_API_USER = os.getenv("FLICKR_API_USER", default="")
 
 # DJANGO ANYMAIL
 ANYMAIL = {
-    "MAILGUN_API_KEY": os.getenv('MAILGUN_API_KEY', default=''),
-    "MAILGUN_SENDER_DOMAIN": os.getenv('MAILGUN_SENDER_DOMAIN', default=''),
+    "MAILGUN_API_KEY": os.getenv("MAILGUN_API_KEY", default=""),
+    "MAILGUN_SENDER_DOMAIN": os.getenv("MAILGUN_SENDER_DOMAIN", default=""),
 }
 EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', default='')
-SERVER_EMAIL = os.getenv('SERVER_EMAIL', default='')
-COMMENT_ADMIN_EMAIL = os.getenv('COMMENT_ADMIN_EMAIL', default='')
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", default="")
+SERVER_EMAIL = os.getenv("SERVER_EMAIL", default="")
+COMMENT_ADMIN_EMAIL = os.getenv("COMMENT_ADMIN_EMAIL", default="")
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-PREPEND_WWW = os.getenv('PREPEND_WWW', default=False)
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+PREPEND_WWW = os.getenv("PREPEND_WWW", default=False)
 SITE_ID = 1
 
 SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'APP': {
-            'client_id': os.getenv('GOOGLE_CLIENT_ID', default=''),
-            'secret': os.getenv('GOOGLE_CLIENT_SECRET', default=''),
-            'key': ''
+    "google": {
+        "APP": {
+            "client_id": os.getenv("GOOGLE_CLIENT_ID", default=""),
+            "secret": os.getenv("GOOGLE_CLIENT_SECRET", default=""),
+            "key": "",
         }
     },
 }
 
-LOGIN_URL='/login/'
-LOGOUT_REDIRECT_URL='/logged-out/'
-ACCOUNT_ADAPTER='page.adapter.MyAccountAdapter'
-SOCIALACCOUNT_LOGIN_ON_GET=True
+LOGIN_URL = "/login/"
+LOGOUT_REDIRECT_URL = "/logged-out/"
+ACCOUNT_ADAPTER = "page.adapter.MyAccountAdapter"
+SOCIALACCOUNT_LOGIN_ON_GET = True
 
 # Make low-quality but small images
 WAGTAILIMAGES_JPEG_QUALITY = 40
 WAGTAILIMAGES_WEBP_QUALITY = 45
 WAGTAIL_ENABLE_WHATS_NEW_BANNER = False
-WAGTAILEMBEDS_FINDERS = [
-    {
-        'class': 'wagtail.embeds.finders.oembed'
-    }
-]
+WAGTAILEMBEDS_FINDERS = [{"class": "wagtail.embeds.finders.oembed"}]
