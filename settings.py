a2336cc5369af6beb6218f2fc61e79ac1e7f93cc
Umair Abbasi <umair.abbasi@fourfridays.com>
2023-12-27 09:54:37 -0500

    dropped prepend_www setting

--- settings.py
+++ settings.py
@@ -198,7 +198,6 @@ DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", default="")
 SERVER_EMAIL = os.getenv("SERVER_EMAIL", default="")
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
-PREPEND_WWW = os.getenv("PREPEND_WWW", default=False)
 SITE_ID = 1
 
 # Make low-quality but small images
