87aacfdb2f3db91f6647562b3b58a0dc12130a24
Umair Abbasi <umair.abbasi@fourfridays.com>
2024-06-28 20:59:24 -0400

    merged pull and index movies into one

--- ratings/management/commands/pull_movies.py
+++ ratings/management/commands/pull_movies.py
@@ -1,4 +1,6 @@
-import datetime, os, pathlib, requests
+import datetime, os, pathlib, requests, time
+
+from settings import DEBUG
 
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
@@ -7,6 +9,7 @@ from django.core.files.images import ImageFile
 from wagtail.models import Collection
 from wagtail.images.models import Image
 
+from algoliasearch.search_client import SearchClient
 from ratings.models import Cast, MoviesIndexPage, MoviePage, MovieGenre, People
 
 from io import BytesIO
@@ -101,6 +104,29 @@ class Command(BaseCommand):
         except ObjectDoesNotExist:
             return False
 
+    def index_movies(self, movie):
+        algolia_app_id = os.environ.get("ALGOLIA_APP_ID", "")
+        algolia_api = os.environ.get("ALGOLIA_API", "")
+        client = SearchClient.create(algolia_app_id, algolia_api)
+        index = client.init_index("movie_index")
+
+        movie_index = {
+            "objectID": movie.movie_id,
+            "title": movie.title,
+            "description": movie.description,
+            "language": movie.language,
+            "release_date_timestamp": time.mktime(movie.release_date.timetuple()),
+            "release_year": movie.release_date.year,
+            "rating": movie.rating,
+            "poster": movie.poster,
+            "genre": list(movie.genre.values_list("name", flat=True)),
+            "cast": list(
+                movie.cast_set.values_list("cast_member__name", flat=True)
+            ),
+            "url": movie.url,
+        }
+        index.save_object(movie_index)
+
     def handle(self, *args, **options):
         # Check to see if MovieIndexPage exists
         movies_index_page = MoviesIndexPage.objects.live().public().get()
@@ -164,5 +190,10 @@ class Command(BaseCommand):
             movies = MoviePage.objects.live().public().all()
             collection = Collection.objects.get(name="Movies")
             for movie in movies:
+                # If no poster, get poster
                 if not movie.image:
                     self.get_poster(movie, collection)
+                
+                # Index the movie if not debug
+                if not DEBUG:
+                    self.index_movies(movie)
