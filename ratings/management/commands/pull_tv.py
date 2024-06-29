e82e726bf8a536baca3d88e26e14cd6980afee2a
Umair Abbasi <umair.abbasi@fourfridays.com>
2024-06-28 22:21:46 -0400

    merged pull and index to one

--- ratings/management/commands/pull_tv.py
+++ ratings/management/commands/pull_tv.py
@@ -1,4 +1,6 @@
-import datetime, os, pathlib, requests
+import datetime, os, pathlib, requests, time
+
+from settings import DEBUG
 
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
@@ -7,6 +9,7 @@ from django.core.files.images import ImageFile
 from wagtail.models import Collection
 from wagtail.images.models import Image
 
+from algoliasearch.search_client import SearchClient
 from ratings.models import TvCast, TvIndexPage, TvPage, TvGenre, People
 
 from io import BytesIO
@@ -101,6 +104,28 @@ class Command(BaseCommand):
         except ObjectDoesNotExist:
             return False
 
+    def index_tv(self, tv):
+        algolia_app_id = os.environ.get("ALGOLIA_APP_ID", "")
+        algolia_api = os.environ.get("ALGOLIA_API", "")
+        client = SearchClient.create(algolia_app_id, algolia_api)
+        index = client.init_index("tv_index")
+
+        tv_index = {
+            "objectID": tv_show.tv_id,
+            "title": tv_show.title,
+            "description": tv_show.description,
+            "language": tv_show.language,
+            "release_date_timestamp": time.mktime(tv_show.release_date.timetuple()),
+            "release_year": tv_show.release_date.year,
+            "rating": tv_show.rating,
+            "poster": tv_show.poster,
+            "genre": list(tv_show.genre.values_list("name", flat=True)),
+            "cast": list(
+                tv_show.tvcast_set.values_list("cast_member__name", flat=True)
+            ),
+            "url": tv_show.url,
+        }
+        index.save_object(tv_index)
     def handle(self, *args, **options):
         # Check to see if TvIndexPage exists
         tv_index_page = TvIndexPage.objects.live().public().get()
@@ -164,3 +189,6 @@ class Command(BaseCommand):
             for tv in tv_shows:
                 if not tv.image:
                     self.get_poster(tv, collection)
+                
+                if not DEBUG:
+                    self.index_tv(tv)
