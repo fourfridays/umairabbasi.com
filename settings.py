8ce78e9c4c58d5fe52c993d48b3beff91d913102
Umair Abbasi <fourfridays@users.noreply.github.com>
2024-08-28 05:14:32 -0400

    another attempt for too many db connections error

--- settings.py
+++ settings.py
@@ -129,6 +129,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite://:memory:")
 
 DATABASES = {"default": dj_database_url.config(
     default=DATABASE_URL,
+    conn_max_age=0,
+    conn_health_checks=False,
 )}
 
 AUTH_PASSWORD_VALIDATORS = [
