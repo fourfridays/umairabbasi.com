9020a84011e178428e64f98327724c62e6fc6a16
Umair Abbasi <umair.abbasi@fourfridays.com>
2025-02-14 14:22:06 -0500

    stupid part 2

--- settings.py
+++ settings.py
@@ -183,23 +183,11 @@ if not "/data/media/" in s3_storage.location:
     STORAGE_BACKEND = "storages.backends.s3boto3.S3Boto3Storage"
     AWS_S3_FILE_OVERWRITE = False
 
-# AWS S3 storage configuration
-AWS_STORAGE_BUCKET_NAME = os.environ.get('DEFAULT_STORAGE_BUCKET', '')
-AWS_ACCESS_KEY_ID = os.environ.get('DEFAULT_STORAGE_ACCESS_KEY_ID', '')
-AWS_SECRET_ACCESS_KEY = os.environ.get('DEFAULT_STORAGE_SECRET_ACCESS_KEY', '')
-AWS_S3_CUSTOM_DOMAIN = os.environ.get('DEFAULT_STORAGE_CUSTOM_DOMAIN', '')
-AWS_S3_REGION_NAME = os.environ.get('DEFAULT_STORAGE_REGION', '')
-AWS_S3_OBJECT_PARAMETERS = {
-    'ACL': 'public-read',
-    'CacheControl': 'max-age=86400',
-}
-AWS_S3_FILE_OVERWRITE = False
-
 # Default storage settings, with the staticfiles storage updated.
 # See https://docs.djangoproject.com/en/5.1/ref/settings/#std-setting-STORAGES
 STORAGE_BACKEND = "django.core.files.storage.FileSystemStorage"
 
-if AWS_SECRET_ACCESS_KEY:
+if AWS_S3_SECRET_ACCESS_KEY:
     STORAGE_BACKEND = "storages.backends.s3boto3.S3Boto3Storage"
 else:
     # only required for local file storage and serving, in development
